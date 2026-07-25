85c85d59794123c43c51084c1d3befdc60f549e2
ksokkhim <khornsokkhim1003@gmail.com>
2026-07-25 20:13:20 +0700

    detail product 60%

---
+++ src/app/(store)/store/[slug]/[slug]/page.tsx
@@ -0,0 +1,16 @@
+
+import DescriptionCard from '@/components/store/productdetail/description-card'
+import ProductDetail from '@/components/store/productdetail/product-detail'
+
+
+export default function page() {
+  return (
+    <div>
+
+        <ProductDetail/>
+        <DescriptionCard/>
+       
+
+    </div>
+  )
+}
