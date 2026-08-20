5ecedf7772b8947040a1aaff32d42ae4d9a9151e
Your Name <you@example.com>
2026-08-20 19:10:52 +0700

    checkout

--- src/app/store/[slug]/product/[productSlug]/page.tsx
+++ src/app/store/[slug]/product/[productSlug]/page.tsx
@@ -127,7 +127,7 @@ export default function DetailProductPage({
             </Link>
           </div>
 
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             {relatedStoreItems.map((item) => (
               <MenuProductCard key={item.id} item={toMenuItem(item, currency)} />
             ))}
